7c36e585184bc60f18f43a9d5ecee329d57ded98
Georgios Alexandros Georgantzas <alexgeorgantzas@outlook.com>
2024-05-25 17:15:53 +0300

    Added Indexes

--- Scripts/drafr_ddl_2.sql
+++ Scripts/drafr_ddl_2.sql
@@ -10,6 +10,7 @@
 -- -- Re-enable foreign key checks after operations
 -- SET FOREIGN_KEY_CHECKS = 1;
 
+
 DROP DATABASE IF EXISTS RecipeDB;
 
 -- Create database
@@ -43,11 +44,12 @@ CREATE TABLE food_group (
     image_ID INT NOT NULL UNIQUE,
     food_group_name VARCHAR(255) NOT NULL UNIQUE,
     food_group_desc TEXT ,
-    category VARCHAR(255) NOT NULL, -- UNIQUE?
+    category VARCHAR(255) NOT NULL, /*UNIQUE?*/
+    INDEX fk_image_id_idx (image_ID ASC),
     FOREIGN KEY (image_ID) REFERENCES images(image_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
--- Ingredients table
+-- Ingredients table 
 CREATE TABLE ingredient (
     ingredient_ID INT AUTO_INCREMENT PRIMARY KEY,
     food_group_ID INT NOT NULL UNIQUE,
@@ -55,6 +57,7 @@ CREATE TABLE ingredient (
     ingr_name VARCHAR(255) NOT NULL UNIQUE ,
     calories DECIMAL(10, 2) NOT NULL CHECK( calories >= 0 AND calories <= 1000),
     unit ENUM('mL', 'L', 'g' ,'table spoon' , 'tea spoon' , 'N/A'),
+    UNIQUE INDEX ingr_group_image_idx (ingredient_ID ASC, food_group_ID ASC, image_ID ASC),
     FOREIGN KEY (image_ID) REFERENCES images(image_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (food_group_ID) REFERENCES food_group(food_group_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
@@ -65,6 +68,7 @@ CREATE TABLE equipment (
     image_ID INT NOT NULL UNIQUE,
     eq_name VARCHAR(255) NOT NULL UNIQUE,
     instructions TEXT NOT NULL,
+    INDEX fk_image_id_idx (image_ID ASC),
     FOREIGN KEY (image_ID) REFERENCES images(image_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
@@ -85,6 +89,7 @@ CREATE TABLE cook (
     age INT NOT NULL CHECK(age >= 18 AND age <= 100),
     experience INT NOT NULL CHECK(experience >= 0 AND experience <=100),  
     rank ENUM('cook C', 'cook B' , 'cook A' , 'assistant chef' , 'chef')  NOT NULL,  
+    INDEX fk_image_id_idx (image_ID ASC),
     FOREIGN KEY (image_ID) REFERENCES images(image_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
@@ -105,7 +110,7 @@ CREATE TABLE recipe (
     prep_time INT NOT NULL CHECK(prep_time>=0),
     cook_time INT NOT NULL CHECK(cook_time>=0),
     portions INT NOT NULL CHECK(portions>=1),
-    tip_1 TEXT , -- isos kai auto varchar
+    tip_1 TEXT , /* isos kai auto varchar*/
     tip_2 TEXT ,
     tip_3 TEXT ,
     fat DECIMAL(10, 1) NOT NULL,
@@ -115,6 +120,9 @@ CREATE TABLE recipe (
     CONSTRAINT t1_not_t2 CHECK (tip_1 <> tip_2),
     CONSTRAINT t1_not_t3 CHECK (tip_1 <> tip_3),
     CONSTRAINT t3_not_t2 CHECK (tip_2 <> tip_3),
+    UNIQUE INDEX recipe_cuisine_main_ingr (recipe_ID ASC, national_cuisine_ID ASC, main_ingredient_ID ASC),
+    INDEX fk_main_ingredient_ID (main_ingredient_ID ASC),
+    INDEX national_cuisine_ID (national_cuisine_ID ASC),
     FOREIGN KEY (national_cuisine_ID) REFERENCES national_cuisine(national_cuisine_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (main_ingredient_ID) REFERENCES ingredient(ingredient_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
@@ -124,6 +132,7 @@ CREATE TABLE meal_recipe (
     meal_type_ID INT NOT NULL,
     recipe_ID INT NOT NULL,
     PRIMARY KEY (meal_type_ID, recipe_ID),
+    -- UNIQUE INDEX 'meal_type_recipe_idx' ('meal_type_ID' ASC, 'recipe_ID' ASC),
     FOREIGN KEY (meal_type_ID) REFERENCES meal_type(meal_type_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (recipe_ID) REFERENCES recipe(recipe_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
@@ -139,6 +148,7 @@ CREATE TABLE label_recipe (
     label_ID INT NOT NULL,
     recipe_ID INT NOT NULL,
     PRIMARY KEY (label_ID, recipe_ID),
+    -- UNIQUE INDEX 'label_recipe_idx' ('label_ID' ASC, 'recipe_ID' ASC),
     FOREIGN KEY (label_ID) REFERENCES label(label_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (recipe_ID) REFERENCES recipe(recipe_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
@@ -147,7 +157,9 @@ CREATE TABLE label_recipe (
 CREATE TABLE steps (
     step_ID INT AUTO_INCREMENT PRIMARY KEY,
     recipe_ID INT NOT NULL,
+    sequence_number INT NOT NULL,
     step_description TEXT NOT NULL,
+    UNIQUE INDEX steps_recipe (step_ID ASC, recipe_ID ASC, sequence_number ASC), /*Maybe Primary Key?*/
     FOREIGN KEY (recipe_ID) REFERENCES recipe(recipe_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
@@ -161,16 +173,18 @@ CREATE TABLE user_table (
 
 -- Episodes table
 CREATE TABLE episode (
-    ep_ID INT AUTO_INCREMENT PRIMARY KEY ,
+    ep_ID INT AUTO_INCREMENT PRIMARY KEY,
     image_ID INT NOT NULL,
     judge_1 INT NOT NULL,
     judge_2 INT NOT NULL,
     judge_3 INT NOT NULL,
-    CONSTRAINT j1_not_j2 CHECK (judge_1 <> judge_2),
-    CONSTRAINT j2_not_j3 CHECK (judge_2 <> judge_3),
-    CONSTRAINT j3_not_j1 CHECK (judge_3 <> judge_1),
     season INT NOT NULL CHECK(season >=1),
     episode INT NOT NULL CHECK(episode >=1 AND episode<=10),
+    -- maybe needs a unique index that combines all FKs
+    INDEX fk_image_ID_idx (image_ID ASC),
+    INDEX fk_judge_1_idx (judge_1 ASC),
+    INDEX fk_judge_2_idx (judge_2 ASC),
+    INDEX fk_judge_3_idx (judge_3 ASC),
     FOREIGN KEY (image_ID) REFERENCES images(image_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (judge_1) REFERENCES cook(cook_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (judge_2) REFERENCES cook(cook_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -183,6 +197,7 @@ CREATE TABLE theme (
     image_ID INT NOT NULL UNIQUE,
     theme_name TEXT NOT NULL UNIQUE,
     theme_desc TEXT NOT NULL UNIQUE,
+    INDEX fk_image_ID_idx (image_ID ASC),
     FOREIGN KEY (image_ID) REFERENCES images(image_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
@@ -191,6 +206,9 @@ CREATE TABLE theme_recipe (
     theme_ID INT NOT NULL,
     recipe_ID INT NOT NULL,
     PRIMARY KEY (theme_ID, recipe_ID),
+    /*UNIQUE INDEX 'theme_recipe_idx' ('theme_ID' ASC, 'recipe_ID' ASC),*/
+    INDEX fk_theme_ID_idx (theme_ID ASC),
+    INDEX fk_recipe_ID_idx (recipe_ID ASC),
     FOREIGN KEY (theme_ID) REFERENCES theme(theme_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (recipe_ID) REFERENCES recipe(recipe_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
@@ -201,6 +219,8 @@ CREATE TABLE recipe_ingr (
     ingredient_ID INT NOT NULL,
     quantity VARCHAR(255) NOT NULL,
     PRIMARY KEY (recipe_ID, ingredient_ID),
+    INDEX fk_recipe_ID_idx (recipe_ID ASC),
+    INDEX fk_ingr_ID_idx (ingredient_ID ASC),
     FOREIGN KEY (recipe_ID) REFERENCES recipe(recipe_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (ingredient_ID) REFERENCES ingredient(ingredient_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
@@ -210,6 +230,8 @@ CREATE TABLE recipe_eq (
     eq_ID INT NOT NULL ,
     recipe_ID INT NOT NULL,
     PRIMARY KEY (eq_ID, recipe_ID),
+    INDEX fk_recipe_ID_idx (recipe_ID ASC),
+    INDEX fk_eq_ID_idx (eq_ID ASC),
     FOREIGN KEY (eq_ID) REFERENCES equipment(eq_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (recipe_ID) REFERENCES recipe(recipe_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
@@ -219,6 +241,8 @@ CREATE TABLE expertise (
     cook_ID INT NOT NULL,
     national_cuisine_ID INT NOT NULL,
     PRIMARY KEY (cook_ID, national_cuisine_ID),
+    INDEX fk_cook_ID_idx (cook_ID ASC),
+    INDEX fk_cuisine_ID_idx (national_cuisine_ID ASC),
     FOREIGN KEY (cook_ID) REFERENCES cook(cook_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (national_cuisine_ID) REFERENCES national_cuisine(national_cuisine_ID) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
@@ -231,8 +255,12 @@ CREATE TABLE ep_info (
     rating_1 TINYINT NOT NULL CHECK (rating_1 >=1 AND rating_1 <=5),
     rating_2 TINYINT NOT NULL CHECK (rating_2 >=1 AND rating_2 <=5),
     rating_3 TINYINT NOT NULL CHECK (rating_3 >=1 AND rating_3 <=5),
-    avg_rating DECIMAL(10, 2) NOT NULL, -- ti tha kanoume ???
+    avg_rating DECIMAL(10, 2) NOT NULL, /*ti tha kanoume ???*/
     PRIMARY KEY (ep_ID, cook_ID),
+    UNIQUE INDEX ep_info_idx (ep_ID ASC, cook_ID ASC, recipe_ID ASC),
+    INDEX ep_ID_idx (ep_ID ASC),
+    INDEX fk_cook_ID_idx (cook_ID ASC),
+    INDEX fk_recipe_ID_idx (recipe_ID ASC),
     FOREIGN KEY (ep_ID) REFERENCES episode(ep_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (cook_ID) REFERENCES cook(cook_ID) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (recipe_ID) REFERENCES recipe(recipe_ID) ON DELETE RESTRICT ON UPDATE CASCADE
