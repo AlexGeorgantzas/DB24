873924aaddbbb3ea00574a9f193ec6206adffdbe
AlexGeorgantzas <alexgeorgantzas@outlook.com>
2024-05-26 23:57:41 +0300

    Finished

--- Scripts/VIEWS.sql
+++ Scripts/VIEWS.sql
@@ -5,11 +5,29 @@ INNER JOIN ep_info ON ep_info.cook_id = cook.cook_id
 GROUP BY cook.cook_ID;
 
 CREATE VIEW 3_1_2 AS
-SELECT national_cuisine.national_cuisine_ID, national_cuisine.cuisine_name, AVG(rating_1 + rating_2 + rating_3) 
+SELECT national_cuisine.national_cuisine_ID, national_cuisine.cuisine_name, AVG(rating_1 + rating_2 + rating_3) /3 
 FROM ep_info 
 INNER JOIN recipe ON recipe.recipe_id = ep_info.recipe_id
 INNER JOIN national_cuisine ON national_cuisine.national_cuisine_ID = recipe.national_cuisine_ID
 GROUP BY national_cuisine.national_cuisine_ID, national_cuisine.cuisine_name;
+
+CREATE VIEW 3_1 AS
+SELECT 
+    c.cook_ID, 
+    r.national_cuisine_ID, 
+    CONCAT(c.firstname, ' ', c.lastname) AS full_name,
+    nc.cuisine_name,
+    AVG((e.rating_1 + e.rating_2 + e.rating_3) / 3) AS avgrating
+FROM 
+    cook c
+INNER JOIN 
+    ep_info e ON c.cook_ID = e.cook_ID
+INNER JOIN 
+    recipe r ON e.recipe_ID = r.recipe_ID
+INNER JOIN
+    national_cuisine nc ON r.national_cuisine_ID = nc.national_cuisine_ID
+GROUP BY 
+    c.cook_ID, r.national_cuisine_ID, full_name, nc.cuisine_name;
 
 
 CREATE VIEW 3_3 AS
@@ -63,6 +81,8 @@ GROUP BY l1.label_description, l2.label_description
 ORDER BY count DESC
 LIMIT 3;
 
+EXPLAIN SELECT * FROM 3_6_1;
+
 CREATE VIEW 3_6_2 AS
 SELECT STRAIGHT_JOIN l1.label_description AS label1, l2.label_description AS label2, COUNT(*) AS count
 FROM label_recipe lr1
@@ -77,30 +97,7 @@ GROUP BY l1.label_description, l2.label_description
 ORDER BY count DESC
 LIMIT 3;
 
-CREATE VIEW 3_6_3 AS
-SELECT l1.label_description AS label1, l2.label_description AS label2, COUNT(*) AS count
-FROM label_recipe lr1
-JOIN label_recipe lr2 ON lr1.recipe_ID = lr2.recipe_ID AND lr1.label_ID < lr2.label_ID
-JOIN ep_info ei ON lr1.recipe_ID = ei.recipe_ID
-JOIN label l1 ON lr1.label_ID = l1.label_ID
-JOIN label l2 ON lr2.label_ID = l2.label_ID
-GROUP BY l1.label_description, l2.label_description
-ORDER BY count DESC
-LIMIT 3;
-
-CREATE VIEW 3_6_4 AS
-SELECT STRAIGHT_JOIN l1.label_description AS label1, l2.label_description AS label2, COUNT(*) AS count
-FROM label_recipe lr1
-FORCE INDEX (recipe_label_idx)
-JOIN label_recipe lr2 FORCE INDEX (recipe_label_idx)
-ON lr1.recipe_ID = lr2.recipe_ID AND lr1.label_ID < lr2.label_ID
-JOIN ep_info ei FORCE INDEX (fk_recipe_ID_idx)
-ON lr1.recipe_ID = ei.recipe_ID
-JOIN label l1 ON lr1.label_ID = l1.label_ID
-JOIN label l2 ON lr2.label_ID = l2.label_ID
-GROUP BY l1.label_description, l2.label_description
-ORDER BY count DESC
-LIMIT 3;
+EXPLAIN SELECT * FROM 3_6_2;
 
 
 CREATE VIEW 3_7 AS
@@ -122,7 +119,8 @@ HAVING
                       JOIN cook c2 ON ei2.cook_ID = c2.cook_ID
                       GROUP BY c2.cook_ID
                       ORDER BY COUNT(ei2.ep_ID) DESC
-                      LIMIT 1) - 5;
+                      LIMIT 1) - 5
+ORDER BY appearances DESC ;
 
 CREATE VIEW 3_8_1 AS
 SELECT e.ep_ID, e.season, e.episode, COUNT(re.eq_ID) AS equipment_count
@@ -133,26 +131,10 @@ GROUP BY e.ep_ID, e.season, e.episode
 ORDER BY equipment_count DESC
 LIMIT 1;
 
+EXPLAIN SELECT * FROM 3_8_1;
+
+
 CREATE VIEW 3_8_2 AS
-SELECT STRAIGHT_JOIN e.ep_ID, e.season, e.episode, COUNT(re.eq_ID) AS equipment_count
-FROM episode e
-JOIN ep_info ei FORCE INDEX (ep_ID_idx) ON e.ep_ID = ei.ep_ID
-JOIN recipe_eq re FORCE INDEX (fk_recipe_ID_idx) ON ei.recipe_ID = re.recipe_ID
-GROUP BY e.ep_ID, e.season, e.episode
-ORDER BY equipment_count DESC;
-
-
-CREATE VIEW 3_8_3 AS
-SELECT e.ep_ID, e.season, e.episode, COUNT(re.eq_ID) AS equipment_count
-FROM episode e
-JOIN ep_info ei ON e.ep_ID = ei.ep_ID
-JOIN recipe_eq re ON ei.recipe_ID = re.recipe_ID
-GROUP BY e.ep_ID, e.season, e.episode
-ORDER BY equipment_count DESC
-LIMIT 1;
-
-
-CREATE VIEW 3_8_4 AS
 SELECT STRAIGHT_JOIN e.ep_ID, e.season, e.episode, COUNT(re.eq_ID) AS equipment_count
 FROM episode e
 JOIN ep_info ei FORCE INDEX (ep_ID_idx)
@@ -163,6 +145,7 @@ GROUP BY e.ep_ID, e.season, e.episode
 ORDER BY equipment_count DESC
 LIMIT 1;
 
+EXPLAIN SELECT * FROM 3_8_2;
 
 
 CREATE VIEW 3_9 AS
@@ -178,28 +161,60 @@ JOIN
 GROUP BY 
     e.season;
 
-CREATE VIEW 3_10 AS
-SELECT 
-    CONCAT(e1.season, '-', e2.season) AS consecutive_seasons,
-    nc.cuisine_name AS national_cuisine,
-    COUNT(*) AS appearances
-FROM 
-    episode e1
-JOIN 
-    episode e2 ON e1.season + 1 = e2.season AND e1.episode = e2.episode
-JOIN 
-    ep_info ei1 ON e1.ep_ID = ei1.ep_ID
-JOIN 
-    ep_info ei2 ON e2.ep_ID = ei2.ep_ID
-JOIN 
-    national_cuisine nc ON ei1.recipe_ID = ei2.recipe_ID
-WHERE 
-    ei1.rating_1 + ei1.rating_2 + ei1.rating_3 > 3
-    AND ei2.rating_1 + ei2.rating_2 + ei2.rating_3 > 3
-GROUP BY 
-    consecutive_seasons, nc.national_cuisine_ID
-ORDER BY 
-    consecutive_seasons, appearances DESC;
+
+CREATE VIEW 3_10 AS 
+select aa.national_cuisine_ID,bb.national_cuisine_ID b_national_cuisine_id,bb.count_in_2,aa.a_season,aa.b_season from 
+(
+select a.national_cuisine_ID, a.season a_season,b.season b_season,a.c+b.c count_in_2 from
+(
+select national_cuisine_ID,season,count(*) c from ep_info
+inner join episode on ep_info.ep_id = episode.ep_id
+inner join recipe on ep_info.recipe_id = recipe.recipe_id
+group by national_cuisine_ID,season 
+having count(*) >2
+)
+A
+cross join 
+(
+select national_cuisine_ID,season,count(*) c from ep_info
+inner join episode on ep_info.ep_id = episode.ep_id
+inner join recipe on ep_info.recipe_id = recipe.recipe_id
+group by national_cuisine_ID,season 
+having count(*) >2
+)
+B
+where a.national_cuisine_ID = b.national_cuisine_ID
+ and a.season = b.season-1
+)aa
+
+JOIN
+
+(
+select a.national_cuisine_ID, a.season a_season,b.season b_season,a.c+b.c count_in_2 from
+(
+select national_cuisine_ID,season,count(*) c from ep_info
+inner join episode on ep_info.ep_id = episode.ep_id
+inner join recipe on ep_info.recipe_id = recipe.recipe_id
+group by national_cuisine_ID,season 
+having count(*) >2
+)
+A
+cross join 
+(
+select national_cuisine_ID,season,count(*) c from ep_info
+inner join episode on ep_info.ep_id = episode.ep_id
+inner join recipe on ep_info.recipe_id = recipe.recipe_id
+group by national_cuisine_ID,season 
+having count(*) >2
+)
+B
+where a.national_cuisine_ID = b.national_cuisine_ID
+ and a.season = b.season-1
+)bb
+
+where aa.count_in_2 = bb.count_in_2 and aa.a_season = bb.a_season
+and aa.b_season = bb.b_season
+and aa.national_cuisine_ID <> bb.national_cuisine_ID;
 
 
 CREATE VIEW 3_11 AS
@@ -229,49 +244,112 @@ ORDER BY
 LIMIT 5;
 
 CREATE VIEW 3_12 AS
-SELECT episode_difficulty.ep_ID
-FROM (
-    SELECT e.ep_ID, SUM(r.difficulty_level) AS total_difficulty
-    FROM episode e
-    INNER JOIN ep_info ei ON e.ep_ID = ei.ep_ID
-    INNER JOIN recipe r ON ei.recipe_ID = r.recipe_ID
-    GROUP BY e.ep_ID
-) AS episode_difficulty
-ORDER BY episode_difficulty.total_difficulty DESC
-LIMIT 1;
+WITH EpisodeDifficulty AS (
+    SELECT 
+        e.season,
+        e.episode,
+        SUM(r.difficulty_level) AS total_difficulty
+    FROM 
+        ep_info ei
+    JOIN 
+        episode e ON ei.ep_ID = e.ep_ID
+    JOIN 
+        recipe r ON ei.recipe_ID = r.recipe_ID
+    GROUP BY 
+        e.season, e.episode
+),
+MaxDifficultyPerSeason AS (
+    SELECT 
+        season,
+        MAX(total_difficulty) AS max_difficulty
+    FROM 
+        EpisodeDifficulty
+    GROUP BY 
+        season
+)
+SELECT 
+    ed.season,
+    ed.episode,
+    ed.total_difficulty
+FROM 
+    EpisodeDifficulty ed
+JOIN 
+    MaxDifficultyPerSeason mds ON ed.season = mds.season AND ed.total_difficulty = mds.max_difficulty
+ORDER BY 
+    ed.season, ed.episode;
 
 
 CREATE VIEW 3_13 AS
-SELECT episode, season,avg_rank_level
-FROM (
-    SELECT e.ep_ID AS episode, e.season,
-           (SUM(CASE j.rank 
-                    WHEN 'cook C' THEN 1 
-                    WHEN 'cook B' THEN 2 
-                    WHEN 'cook A' THEN 3 
-                    WHEN 'assistant chef' THEN 4 
-                    WHEN 'chef' THEN 5 
-                    ELSE 0
-                END) + 
-                SUM(CASE c.rank 
-                    WHEN 'cook C' THEN 1 
-                    WHEN 'cook B' THEN 2 
-                    WHEN 'cook A' THEN 3 
-                    WHEN 'assistant chef' THEN 4 
-                    WHEN 'chef' THEN 5 
-                    ELSE 0
-                END)) / 13 AS avg_rank_level
-    FROM episode e
-    INNER JOIN cook j ON e.judge_1 = j.cook_ID OR e.judge_2 = j.cook_ID OR e.judge_3 = j.cook_ID
-    INNER JOIN ep_info ei ON e.ep_ID = ei.ep_ID
-    INNER JOIN cook c ON ei.cook_ID = c.cook_ID
-    GROUP BY e.ep_ID, e.season
-) AS avg_ranks_per_episode
-ORDER BY avg_rank_level ASC
-LIMIT 1;
+WITH ranked_cooks_and_judges AS (
+    SELECT 
+        e.ep_ID,
+        e.season,
+        CASE 
+            WHEN c.rank = 'cook C' THEN 1 
+            WHEN c.rank = 'cook B' THEN 2 
+            WHEN c.rank = 'cook A' THEN 3 
+            WHEN c.rank = 'assistant chef' THEN 4 
+            WHEN c.rank = 'chef' THEN 5 
+            ELSE 0 
+        END AS rank
+    FROM 
+        ep_info ei
+    JOIN 
+        cook c ON ei.cook_ID = c.cook_ID
+    JOIN 
+        episode e ON ei.ep_ID = e.ep_ID
+
+    UNION ALL
+
+    SELECT 
+        e.ep_ID,
+        e.season,
+        CASE 
+            WHEN j.rank = 'cook C' THEN 1 
+            WHEN j.rank = 'cook B' THEN 2 
+            WHEN j.rank = 'cook A' THEN 3 
+            WHEN j.rank = 'assistant chef' THEN 4 
+            WHEN j.rank = 'chef' THEN 5 
+            ELSE 0 
+        END AS rank
+    FROM 
+        episode e
+    JOIN 
+        cook j ON j.cook_ID IN (e.judge_1, e.judge_2, e.judge_3)
+),
+episode_avg_ranks AS (
+    SELECT 
+        ep_ID,
+        season,
+        AVG(rank) AS average_rank
+    FROM 
+        ranked_cooks_and_judges
+    GROUP BY 
+        ep_ID, season
+),
+ranked_episodes AS (
+    SELECT 
+        ep_ID,
+        season,
+        average_rank,
+        RANK() OVER (PARTITION BY season ORDER BY average_rank DESC) AS rank_within_season
+    FROM 
+        episode_avg_ranks
+)
+SELECT 
+    ep_ID,
+    season,
+    average_rank
+FROM 
+    ranked_episodes
+WHERE 
+    rank_within_season = 1
+ORDER BY 
+    season;
+
 
 CREATE VIEW 3_14 AS
-SELECT theme_ID, theme_name, c
+SELECT theme_name, c as appearances
 FROM (
     SELECT theme.theme_ID, theme_name, COUNT(DISTINCT ep_id) AS c
     FROM theme
@@ -280,6 +358,7 @@ FROM (
     INNER JOIN ep_info ON ep_info.recipe_id = recipe.recipe_id
     GROUP BY theme_ID, theme_name
     ORDER BY c DESC
+    LIMIT 1
 ) AS xx;
 
 CREATE VIEW 3_15 AS
--- Scripts/season.py
+++ Scripts/season.py
@@ -4,15 +4,14 @@ import random
 import logging
 
 logging.basicConfig(
-    level=logging.DEBUG,  # Set to DEBUG to capture all levels of logs
+    level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("recipe_db.log"),  # Specify the log file name
-        logging.StreamHandler()  # Also output to console
+        logging.FileHandler("recipe_db.log"),
+        logging.StreamHandler()
     ]
 )
 
-# MySQL connection using mysql.connector
 try:
     userName = "root"
     pswd = "2255"
@@ -33,7 +32,6 @@ try:
         # SQL queries
         get_all_cuisines_query = "SELECT national_cuisine_ID FROM national_cuisine"
         get_all_cooks_query = "SELECT cook_ID FROM cook"
-        get_all_recipes_query = "SELECT recipe_ID, national_cuisine_ID FROM recipe"
         get_expertise_query = "SELECT cook_ID, national_cuisine_ID FROM expertise"
 
         # Execute SQL queries and fetch results
@@ -43,14 +41,15 @@ try:
         cursor.execute(get_all_cooks_query)
         all_cooks = [row[0] for row in cursor.fetchall()]
 
-        cursor.execute(get_all_recipes_query)
-        all_recipes = [(row[0], row[1]) for row in cursor.fetchall()]
-
         cursor.execute(get_expertise_query)
         expertise = [(row[0], row[1]) for row in cursor.fetchall()]
 
-        def is_valid_selection(selection, history):
-            return history.count(selection) < 3
+        def is_valid_selection(selected_id, history, ep_num):
+            count = 0
+            for i in range(len(history)):
+                if selected_id in history[0]:
+                    count +=1
+            return count < 3
 
         def update_history(history, selection):
             history.append(selection)
@@ -58,63 +57,121 @@ try:
                 history.pop(0)
 
         def create_new_season(year, num_episodes=10):
-            cursor.execute("SELECT IFNULL(MAX(season), 0) + 1 FROM episode")
-            new_season_number = cursor.fetchone()[0]
-            cuisine_history, recipe_history, cook_history = [], [], []
+            try:
+                cursor.execute("SELECT IFNULL(MAX(season), 0) + 1 FROM episode")
+                new_season_number = cursor.fetchone()[0]
+            except Error as e:
+                logging.error(f"Error retrieving new season number: {e}")
+                return
+
+            cuisine_history, recipe_history, cook_judge_history = [], [], []
 
             for episode_num in range(1, num_episodes + 1):
+                cuisines = []
+                for cuis in all_cuisines:
+                    cuisines.append(cuis)
                 try:
                     selected_cuisines = []
                     selected_recipes = []
                     selected_cooks = []
-
-                    # Select 10 random cuisines
-                    while len(selected_cuisines) < 10:
-                        cuisine = random.choice(all_cuisines)
-                        if is_valid_selection(cuisine, cuisine_history):
-                            selected_cuisines.append(cuisine)
-                            update_history(cuisine_history, cuisine)
+                    selected_judges = []
+                    # Select 10 unique random cuisines
+                    if len(all_cuisines) < 10:
+                        logging.error("Not enough cuisines available to select 10 unique cuisines.")
+                        continue  # Skip to the next episode
+                    for i in range(10):
+                        while(True):
+                            cuisine_ID = random.choice(cuisines)
+                            #print('1: ', is_valid_selection(cuisine_ID, cuisine_history, episode_num))
+                            #print('2: ',(cuisine_ID not in selected_cuisines))
+                            #print(f'Episode {episode_num}: ', is_valid_selection(cuisine_ID, cuisine_history, episode_num) and (cuisine_ID not in selected_cuisines) and cuisine_ID != 20)
+                            if(is_valid_selection(cuisine_ID, cuisine_history, episode_num) and (cuisine_ID not in selected_cuisines)):
+                                selected_cuisines.append(cuisine_ID)
+                                cuisines.remove(cuisine_ID)
+                                #print(selected_cuisines)
+                                break
+                    update_history(cuisine_history, selected_cuisines)
 
                     logging.debug(f"Episode {episode_num}: Selected cuisines: {selected_cuisines}")
 
-                    # Select 1 recipe from each selected cuisine
+                    # Select 1 recipe from each selected cuisine using SQL JOIN
                     for cuisine in selected_cuisines:
-                        valid_recipes = [recipe for recipe in all_recipes if recipe[1] == cuisine]
-                        logging.debug(f"Episode {episode_num}: Valid recipes for cuisine {cuisine}: {valid_recipes}")
-                        if valid_recipes:  # Check if valid_recipes is not empty
-                            recipe = random.choice(valid_recipes)[0]
-                            if is_valid_selection(recipe, recipe_history):
-                                selected_recipes.append(recipe)
-                                update_history(recipe_history, recipe)
-                        else:
-                            logging.warning(f"No recipes found for cuisine ID: {cuisine}")
-                            continue  # Skip to the next episode
+                        try:
+                            while(True):
+                                query = """
+                                SELECT r.recipe_ID 
+                                FROM recipe AS r
+                                JOIN national_cuisine AS nc ON r.national_cuisine_ID = nc.national_cuisine_ID
+                                WHERE r.national_cuisine_ID = %s
+                                ORDER BY RAND()
+                                LIMIT 1
+                                """
+                                cursor.execute(query, (cuisine,))
+                                recipe_ID = [row[0] for row in cursor.fetchall()][0]
+                                if is_valid_selection(recipe_ID, recipe_history, episode_num) and recipe_ID not in selected_recipes:
+                                    selected_recipes.append(recipe_ID)
+                                    break
+                                else:
+                                    #logging.warning(f"No recipes found for cuisine ID: {cuisine}")
+                                    continue  # Skip to the next cuisine
+                        except Error as e:
+                            logging.error(f"Error retrieving recipes for cuisine {cuisine}: {e}")
+                            continue  # Skip to the next cuisine
 
+                    update_history(recipe_history, selected_recipes)
                     logging.debug(f"Episode {episode_num}: Selected recipes: {selected_recipes}")
 
                     # Select 10 cooks based on their expertise
                     for cuisine in selected_cuisines:
-                        valid_cooks = [cook for cook in expertise if cook[1] == cuisine]
-                        logging.debug(f"Episode {episode_num}: Valid cooks for cuisine {cuisine}: {valid_cooks}")
-                        if valid_cooks:  # Check if valid_cooks is not empty
-                            cook = random.choice(valid_cooks)[0]
-                            if is_valid_selection(cook, cook_history):
-                                selected_cooks.append(cook)
-                                update_history(cook_history, cook)
-                        else:
-                            logging.warning(f"No cooks found with expertise in cuisine ID: {cuisine}")
-                            continue  # Skip to the next episode
-
+                        while(True):
+                            query = """
+                                SELECT c.cook_ID
+                                FROM cook AS c
+                                JOIN (national_cuisine AS nc JOIN expertise as e ON e.national_cuisine_ID = nc.national_cuisine_ID) ON c.cook_ID = e.cook_ID
+                                WHERE nc.national_cuisine_ID = %s
+                                ORDER BY RAND()
+                                LIMIT 1;
+                                """
+                            cursor.execute(query, (cuisine,))
+                            cook_ID = [row[0] for row in cursor.fetchall()][0]
+                            if is_valid_selection(cook_ID, cook_judge_history, episode_num) and cook_ID not in selected_cooks:
+                                selected_cooks.append(cook_ID)
+                                break
+                            else:
+                                logging.warning(f"Duplicate cook found with expertise in cuisine ID: {cuisine}. Searching again...")
+                                continue  # Skip to the next cuisine
                     logging.debug(f"Episode {episode_num}: Selected cooks: {selected_cooks}")
+                    
+                    selected_cooks_judges = selected_cooks
 
-                    # Insert new episode
-                    judges = random.sample(all_cooks, 3)
+                    # Select Judges
+                    for i in range(3):
+                        while(True):
+                            get_judges_query = """
+                                SELECT cook_ID
+                                FROM cook
+                                WHERE cook_ID NOT IN (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                                ORDER BY RAND()
+                                LIMIT 1;
+                                """
+                            cursor.execute(get_judges_query, (selected_cooks[0], selected_cooks[1], selected_cooks[2], selected_cooks[3], selected_cooks[4], selected_cooks[5], selected_cooks[6], selected_cooks[7], selected_cooks[8], selected_cooks[9]))
+                            valid_judge = [row[0] for row in cursor.fetchall()][0]
+                            if is_valid_selection(valid_judge, cook_judge_history, episode_num) and valid_judge not in selected_cooks_judges:
+                                selected_judges.append(valid_judge)
+                                selected_cooks_judges.append(valid_judge)
+                                break
+                            else:
+                                continue
+
+                    update_history(cook_judge_history, selected_cooks_judges)
+                    logging.debug(f"Episode {episode_num}: Selected judges: {selected_judges}")
+
                     insert_episode_query = """
                     INSERT INTO episode (image_ID, judge_1, judge_2, judge_3, season, episode)
                     VALUES (1, %s, %s, %s, %s, %s)
                     """
                     cursor.execute(insert_episode_query, (
-                        judges[0], judges[1], judges[2], new_season_number, episode_num
+                        selected_judges[0], selected_judges[1], selected_judges[2], new_season_number, episode_num
                     ))
                     conn.commit()
                     new_episode_id = cursor.lastrowid
@@ -134,6 +191,7 @@ try:
                     logging.info(f"Episode {episode_num} for season {new_season_number} created successfully.")
                 except Exception as e:
                     logging.error(f"Error while creating episode {episode_num} for season {new_season_number}: {e}")
+                    conn.rollback()
                     continue  # Skip to the next episode
 
         if __name__ == "__main__":
@@ -142,6 +200,13 @@ try:
 except Error as e:
     logging.error(f"Error while connecting to MariaDB with mysql.connector: {e}")
 finally:
-    if conn.is_connected():
-        cursor.close()
-        conn.close()
+    try:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            logging.info("Database connection closed.")
+    except NameError:
+        logging.error("Connection or cursor not properly initialized.")
+
+
+
